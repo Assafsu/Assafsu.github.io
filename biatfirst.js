40eda0036a19c013ea0ea37c7921ef78b1cd18ee
Assaf Suberry <87972790+Assafsu@users.noreply.github.com>
2021-09-30 19:04:10 +0300

    Update biatfirst.js

--- biatfirst.js
+++ biatfirst.js
@@ -50,48 +50,48 @@ define(['pipAPI', 'https://assafsu.github.io/biatsecond.js'], function(APIConstr
 			},
 			categories : [  //As many categories you need.
 				{
-					name : 'Black People', //Will appear in the data.
+					name : 'זקנים', //Will appear in the data.
 					title : {
-						media : {word : 'Black People'}, //Name of the category presented in the task.
+						media : {word : 'זקנים'}, //Name of the category presented in the task.
 						css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
 						height : 4, //Height (because we need to know where to put the next item in the title)
 						startStimulus : { 
 						//If you're using a startStimulus, set here. If not, set the parameter showStimuliWithInst to false (see later below)
-						media : {image : 'blacks.jpg'}, 
+						media : {image : 'Olds.jpg'}, 
 							css : {color:'#31b404','font-size':'1em'}, 
 							height : 2
 						}
 					}, 
 					stimulusMedia : [ //Stimuli content as PIP's media objects
-					{image : 'black1.jpg'}, 
-        			{image : 'black2.jpg'}, 
-        			{image : 'black3.jpg'}, 
-        			{image : 'black4.jpg'}, 
-        			{image : 'black5.jpg'}, 
-        			{image : 'black6.jpg'}
+					{image : 'of1.jpg'}, 
+        			{image : 'of2.jpg'}, 
+        			{image : 'of3.jpg'}, 
+        			{image : 'om1.jpg'}, 
+        			{image : 'om2.jpg'}, 
+        			{image : 'om3.jpg'}
 					], 
 					//Stimulus css (style of the stimuli)
 					stimulusCss : {color:'#31b404','font-size':'2em'}
 				},	
 				{
-					name : 'White people', 
+					name : 'צעירים', 
 					title : {
-						media : {word : 'White people'}, 
+						media : {word : 'צעירים'}, 
 						css : {color:'#31b404','font-size':'1.8em'}, 
 						height : 4,
 						startStimulus : {
-							media : {image : 'whites.jpg'},  
+							media : {image : 'Youngs.jpg'},  
 							css : {color:'#31b404','font-size':'1em'}, 
 							height : 2
 						}
 					}, 
 					stimulusMedia : [ //Stimuli content as PIP's media objects
 					{image : 'yf1.jpg'}, 
-        			{image : 'yf4.jpg'}, 
-        			{image : 'yf5.jpg'}, 
+        			{image : 'yf2.jpg'}, 
+        			{image : 'yf3.jpg'}, 
+        			{image : 'ym1.jpg'}, 
         			{image : 'ym2.jpg'}, 
-        			{image : 'ym3.jpg'}, 
-        			{image : 'ym5.jpg'}
+        			{image : 'ym3.jpg'}
 					], 
 					//Stimulus css
 					stimulusCss : {color:'#31b404','font-size':'2em'}
