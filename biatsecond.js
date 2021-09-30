b086983ba2fd21c44387090eaa56338cc51d45ef
Assaf Suberry <87972790+Assafsu@users.noreply.github.com>
2021-09-30 18:05:32 +0300

    Update biatsecond.js

--- biatsecond.js
+++ biatsecond.js
@@ -43,45 +43,45 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor,Scorer, _) {
 			}, 
 			practiceCategory1 : 
 			{
-				name : 'Mammals', //Will appear in the data.
+				name : 'יונקים', //Will appear in the data.
 				title : {
-					media : {word : 'Mammals'}, //Name of the category presented in the task.
+					media : {word : 'יונקים'}, //Name of the category presented in the task.
 					css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
 					height : 4, //Height (because we need to know where to put the next item in the title)
 					startStimulus : { 
 					//If you're using a startStimulus, set here. If not, set the parameter showStimuliWithInst to false (see later below)
-						media : {word : 'Dogs, Horses, Cows, Lions'}, 
+						media : {word : 'כלבים, סוסים, פרות, אריות'}, 
 						css : {color:'#31b404','font-size':'1em'}, 
 						height : 2
 					}
 				}, 
 				stimulusMedia : [ //Stimuli content as PIP's media objects
-					{word : 'Dogs'}, 
-					{word : 'Horses'}, 
-					{word : 'Lions'}, 
-					{word : 'Cows'}
+					{word : 'כלבים'}, 
+					{word : 'סוסים'}, 
+					{word : 'אריות'}, 
+					{word : 'פרות'}
 				], 
 				//Stimulus css (style of the stimuli)
 				stimulusCss : {color:'#31b404','font-size':'2em'}
 			},	
 			practiceCategory2 : 
 			{
-				name : 'Birds', 
+				name : 'ציפורים', 
 				title : {
-					media : {word : 'Birds'}, 
+					media : {word : 'ציפורים'}, 
 					css : {color:'#31b404','font-size':'1.8em'}, 
 					height : 4,
 					startStimulus : {
-						media : {word : 'Pigeons, Swans, Crows, Ravens'}, 
+						media : {word : 'יונים, ברבורים, עורבים, דרורים'}, 
 						css : {color:'#31b404','font-size':'1em'}, 
 						height : 2
 					}
 				}, 
 				stimulusMedia : [ //Stimuli content as PIP's media objects
-					{word : 'Pigeons'}, 
-					{word : 'Swans'}, 
-					{word : 'Crows'}, 
-					{word : 'Ravens'}
+					{word : 'יונים'}, 
+					{word : 'ברבורים'}, 
+					{word : 'עורבים'}, 
+					{word : 'דרורים'}
 				], 
 				//Stimulus css
 				stimulusCss : {color:'#31b404','font-size':'2em'}
@@ -129,44 +129,44 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor,Scorer, _) {
 			],
 			attribute1 : 
 			{
-				name : 'Pleasant', 
+				name : 'נעים', 
 				title : {
-					media : {word : 'Pleasant'}, 
+					media : {word : 'נעים'}, 
 					css : {color:'#0000FF','font-size':'1.8em'}, 
 					height : 4,
 					startStimulus : {
-						media : {word : 'Joy, Love, Happy, Good'}, 
+						media : {word : 'אושר ,אהבה, שמחה, טוב'}, 
 						css : {color:'#0000FF','font-size':'1em'}, 
 						height : 2
 					}
 				}, 
 				stimulusMedia : [ //Stimuli content as PIP's media objects
-					{word : 'Joy'}, 
-					{word : 'Love'}, 
-					{word : 'Happy'}, 
-					{word : 'Good'}
+					{word : 'אושר'}, 
+					{word : 'אהבה'}, 
+					{word : 'שמחה'}, 
+					{word : 'טוב'}
 				], 
 				//Stimulus css
 				stimulusCss : {color:'#0000FF','font-size':'2em'}
 			},	
 			attribute2 : 
 			{
-				name : 'Unpleasant', 
+				name : 'לא נעים', 
 				title : {
-					media : {word : 'Unpleasant'}, 
+					media : {word : 'לא נעים'}, 
 					css : {color:'#0000FF','font-size':'1.8em'}, 
 					height : 4,
 					startStimulus : {
-						media : {word : 'Horrible, Evil, Nasty, Bad'}, 
+						media : {word : 'נורא, מרושע, דוחה, רע'}, 
 						css : {color:'#0000FF','font-size':'1em'}, 
 						height : 2
 					}
 				}, 
 				stimulusMedia : [ //Stimuli content as PIP's media objects
-					{word : 'Horrible'}, 
-					{word : 'Nasty'}, 
-					{word : 'Bad'}, 
-					{word : 'Evil'}
+					{word : 'נורא'}, 
+					{word : 'דוחה'}, 
+					{word : 'רע'}, 
+					{word : 'מרושע'}
 				], 
 				//Stimulus css
 				stimulusCss : {color:'#0000FF','font-size':'2em'} 
