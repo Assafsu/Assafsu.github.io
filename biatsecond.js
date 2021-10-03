70c8d036c5f43a35890331dd4a075f67e63e1cbe
Assaf Suberry <87972790+Assafsu@users.noreply.github.com>
2021-10-03 10:29:03 +0300

    Update biatsecond.js

--- biatsecond.js
+++ biatsecond.js
@@ -749,7 +749,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor,Scorer, _) {
 						media:{word:'וגם'}, css:{color:'#000000','font-size':'1.8em'}}, 
 					{location:{top:5 + 1 + (params.focalCatTitle.height | 7) + (params.focalCatTitle.startStimulus.height | 6)}, 
 					media : params.focalAttTitle.media, css: params.focalAttTitle.css},
-					{location:{top:5 + 1 + (params.focalCatTitle.height | 7) + (params.focalCatTitle.startStimulus.height | 3) + (params.focalAttTitle.height | 8)}, 
+					{location:{top:5 + 1 + (params.focalCatTitle.height | 7) + (params.focalCatTitle.startStimulus.height | 4) + (params.focalAttTitle.height | 8)}, 
 					media : params.focalAttTitle.startStimulus.media, css: params.focalAttTitle.startStimulus.css}
 				]); 
 			}
